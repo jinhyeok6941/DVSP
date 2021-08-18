e2dc5e6d7e92d4ea36b59888c3436faa5395a64e
SeungHyunKimm <skyblue051@naver.com>
2021-08-18 14:56:39 +0900

    ..

---
+++ DVSP/Assets/_Main/02.Scripts/ButtonManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+
+
+    public void OnClickStartButton()
+    {
+        
+    }
+
+    public void OnClickPracticeButton()
+    {
+
+    }
+
+    public void OnClickTutorialButton()
+    {
+    
+    }
+
+    
+
+}
