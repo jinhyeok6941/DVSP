dbd56495286a2d90ca2c46132904c89589bc00a8
SeungHyunKimm <skyblue051@naver.com>
2021-08-23 17:10:08 +0900

    ..

---
+++ DVSP/Assets/KSH/02.Scripts/PlayerMove.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed = 10;
+
+    void Start()
+    {
+        
+    }
+
+
+
+    void Update()
+    {
+               
+    }
+
+
+
+
+}
