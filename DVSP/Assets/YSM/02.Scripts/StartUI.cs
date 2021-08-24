9fe45683037b68fc03b05927de35020243f61926
ysm7280 <ysm7280@naver.com>
2021-08-24 17:44:15 +0900

    ..

--- DVSP/Assets/YSM/02.Scripts/StartUI.cs
+++ DVSP/Assets/YSM/02.Scripts/StartUI.cs
@@ -25,13 +25,13 @@ public class StartUI : MonoBehaviour
     void DrawGuideLine()
     {
         //1. 오른손 위치, 오른손 앞방향에서 발사하는 Ray를 만든다.
-        Ray ray = new Ray(rHand.transform.position, rHand.transform.forward);
+        Ray ray = new Ray(transform.position, transform.forward);
         //2. 부딪힌 곳이 있다면
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit))
         {
             //3. 부딪힌 지점까지 Line 을 그린다
-            lr.SetPosition(0, rHand.transform.position);
+            lr.SetPosition(0, transform.position);
             lr.SetPosition(1, hit.point);
             if (hit.transform.gameObject.layer == LayerMask.NameToLayer("UI"))
             {
@@ -41,15 +41,13 @@ public class StartUI : MonoBehaviour
                 img = hit.transform.GetComponent<Image>();
                 img.color = Color.green;
             }
-
         }
         else
         {
             img.color = Color.white;
             //4. 부딪힌 지점이 없으면 오른손위치에서 오른손 앞방향으로 몇미터까지 그려라
-            lr.SetPosition(0, rHand.transform.position);
-            lr.SetPosition(1, rHand.transform.position + rHand.transform.forward * 3);
-     
+            lr.SetPosition(0, transform.position);
+            lr.SetPosition(1, transform.position + transform.forward * 3);
         }
     }
     void pointer()
