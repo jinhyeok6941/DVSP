62c2e0651f5edcd4b12128290cd4c9dac874a4d4
ysm7280 <ysm7280@naver.com>
2021-08-24 14:34:43 +0900

    .

---
+++ DVSP/Assets/YSM/02.Scripts/StartUI.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartUI : MonoBehaviour
+{
+    LineRenderer lr;
+    public Transform lHand;
+    public Transform rHand;
+    public Transform dot;
+
+    void Start()
+    {
+        //lr = gameObject.GetComponentInChildren<LineRenderer>();
+    }
+
+    void Update()
+    {
+        pointer();
+        //DrawGuideLine();
+    }
+
+    void DrawGuideLine()
+    {
+        //1. 오른손 위치, 오른손 앞방향에서 발사하는 Ray를 만든다.
+        Ray ray = new Ray(rHand.transform.position, rHand.transform.forward);
+        //2. 부딪힌 곳이 있다면
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            //3. 부딪힌 지점까지 Line 을 그린다
+            lr.SetPosition(0, transform.position);
+            lr.SetPosition(1, hit.point);
+        }
+        else
+        {
+            //4. 부딪힌 지점이 없으면 오른손위치에서 오른손 앞방향으로 몇미터까지 그려라
+            lr.SetPosition(0, transform.position);
+            lr.SetPosition(1, rHand.transform.position + rHand.transform.forward * 1);
+        }
+    }
+    void pointer()
+    {
+        //오른손 위치에서 오른손 앞방향
+        Ray ray = new Ray(rHand.transform.position, rHand.forward);
+        //부딪혔다면
+        RaycastHit hit;
+
+        //int layer = 1 << LayerMask.NameToLayer("UI");
+        //if(Physics.Raycast(ray, out hit,100))
+        if (Physics.Raycast(ray, out hit))
+        {
+            //만약 부딪힌놈의 layer가 UI라면
+            if (hit.transform.gameObject.layer == LayerMask.NameToLayer("UI"))
+            {
+                //그위치에 빨간점 생성
+                dot.gameObject.SetActive(true);
+                dot.position = hit.point;
+            }
+            else
+            {
+                dot.gameObject.SetActive(false);
+            }
+        }
+        else
+        {
+            dot.gameObject.SetActive(false);
+        }
+
+        if (dot.gameObject.activeSelf == true)
+        {
+            if (OVRInput.GetDown(OVRInput.Button.One, OVRInput.Controller.RTouch))
+            {
+                //btn 스크립트 가져온다
+                Button btn = hit.transform.GetComponent<Button>();
+                if (btn != null)
+                {
+                    btn.onClick.Invoke();
+                }
+            }
+        }
+    }
+}
