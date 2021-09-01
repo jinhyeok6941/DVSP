962d142077503bba231c8f6329b39ee00c9cdf1d
jihye-yu <13116@naver.com>
2021-09-01 10:31:16 +0900

    tutorial01

--- DVSP/Assets/TutorialManager.cs
+++ DVSP/Assets/TutorialManager.cs
@@ -13,30 +13,29 @@ public class TutorialManager : MonoBehaviour
 
     int stepCount;
 
-    public MeshRenderer Steps;
+    //public MeshRenderer Steps;
     void Start()
     {
-        //stepCount = drone.step;
+        SeletStep();
     }
 
     // 단계가 클리어 되면 완료된 단계의 step의 불들어오게 하기.
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            StopAllCoroutines();
-            count++;
-            ChangeTest();
+        //if (Input.GetKeyDown(KeyCode.Alpha1))
+        //{
+        //    StopAllCoroutines();
+        //    count++;
+        //    ChangeTest();
 
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            StopAllCoroutines();
-            count--;
-            ChangeTest();
-        }
-
-
+        //}
+        //if (Input.GetKeyDown(KeyCode.Alpha2))
+        //{
+        //    StopAllCoroutines();
+        //    count--;
+        //    ChangeTest();
+        //}
+        StepNext();
     }
     //단계 변화 감지
     void StepNext()
@@ -45,13 +44,14 @@ public class TutorialManager : MonoBehaviour
         {
             stepCount = drone.step;// 그 단계로 지정해주고 
             StopAllCoroutines();//모든 코루틴 멈춰주고
+            ResetBtn();// 색을 전부 normal로 바꿔주고  
             SeletStep(); //해당 Step에 맞는 행동 실행 
         }
     }
 
     void SeletStep()
     {
-        switch (stepCount % 8)
+        switch (stepCount % 17)
         {
             case 0:
                 guide.text = "좌측 상단 버튼을 3초이상 누르면 드론이 이륙합니다.";
@@ -62,74 +62,65 @@ public class TutorialManager : MonoBehaviour
                 StartCoroutine(PressHard(0));
                 break;
             case 2:
+                guide.text = "완료! 다시 이륙해봐요! ";
+                break;
+            //case 3:
+            //    break; // 3번은 의미 없음 한번만 하기 위해 그냥 있는 숫자 
+            case 4:
                 guide.text = "우측 조이스틱으로 전후좌우 드론을 움직여보세요! ";
                 StartCoroutine(PressBlink(3));
                 break;
-            case 3:
+   ㄴ         case 5:
+                guide.text = "완료! ";
+                break;
+            //case 6:
+            //    break;
+            case 7:
                 guide.text = "좌측 조이스틱으로 드론을 상승 하강시키고 , 좌우로 회전 시킬수 있어요!";
                 StartCoroutine(PressBlink(2));
                 break;
-            case 4:
+            case 8:
+                guide.text = "완료! ";
+                break;
+            //case 9:
+            //    break;
+            case 10:
                 guide.text = "좌측 상단 버튼을 짧게 누르면 속도가 바뀌어요!(3단계)";
                 StartCoroutine(PressBlink(0));
                 break;
-            case 5:
-                guide.text = "뒤집기는  우측 상단의 버튼을 꾹누르면서 우측 조이스틱을 움직이면 움직인방향으로 뒤집어요!";
+            case 11:
+                guide.text = "완료! ";
+                break;
+            case 12:
+                guide.text = "뒤집기는  우측 상단의 버튼을 꾹 3초이상 눌르면서";
+                StartCoroutine(PressHard(1));
+                break;
+            case 13:
+                guide.text = " 버튼을 누르면서 우측 조이스틱을 움직이면 움직인방향으로 뒤집어요!";
                 StartCoroutine(PressHard(1));
                 StartCoroutine(PressBlink(3));
                 break;
-            case 6:
-                guide.text = "착륙해 있을 떄 우층 상단 버튼을 짧게 누르면 드론 색이 바뀌어요!";
+            case 14:
+                guide.text = "완료! ";
+                break;
+            case 15:
+                guide.text = "일단 바닥에 한번 착륙해봐요! ";
+                StartCoroutine(PressHard(0));
+                break;
+            case 16:
+                guide.text = "착륙해있을 떄 우측 상단 버튼을 누르면 드론 색이 바뀌어요! ";
                 StartCoroutine(PressBlink(1));
                 break;
-            case 7:
-                guide.text = "headLess모드로 운전하고싶으면 가운데 버튼을 누르면 되요!";
-                guidePed.ST_Click(JOYPED.BTN_STATE.CLICK, 4);
+            case 17:
+                guide.text = "완료! ";
+                break;
+            default:
                 break;
         }
     }
 
-    void ChangeTest()
-    {
-        guidePed.Guide_Reset();
-        switch (count % 8)
-        {
-            case 0:
-                guide.text = "좌측 상단 버튼을 3초이상 누르면 드론이 이륙합니다.";
-                StartCoroutine(PressHard(0));
-                break;
-            case 1:
-                guide.text = "비행 중에 좌측 상단 버튼을 3초이상 누르면 드론이 착륙합니다.";
-                StartCoroutine(PressHard(0));
-                break;
-            case 2:
-                guide.text = "우측 조이스틱으로 전후좌우 드론을 움직여보세요! ";
-                StartCoroutine(PressBlink(3));
-                break;
-            case 3:
-                guide.text = "좌측 조이스틱으로 드론을 상승 하강시키고 , 좌우로 회전 시킬수 있어요!";
-                StartCoroutine(PressBlink(2));
-                break;
-            case 4:
-                guide.text = "좌측 상단 버튼을 짧게 누르면 속도가 바뀌어요!(3단계)";
-                StartCoroutine(PressBlink(0));
-                break;
-            case 5:
-                guide.text = "뒤집기는  우측 상단의 버튼을 꾹누르면서 우측 조이스틱을 움직이면 움직인방향으로 뒤집어요!";
-                StartCoroutine(PressHard(1));
-                StartCoroutine(PressBlink(3));
-                break;
-            case 6:
-                guide.text = "착륙해 있을 떄 우층 상단 버튼을 짧게 누르면 드론 색이 바뀌어요!";
-                StartCoroutine(PressBlink(1));
-                break;
-            case 7:
-                guide.text = "headLess모드로 운전하고싶으면 가운데 버튼을 누르면 되요!";
-                guidePed.ST_Click(JOYPED.BTN_STATE.CLICK, 4);
-                break;
-        }
-
-    }
+   
+    
 
 
 
@@ -174,6 +165,13 @@ public class TutorialManager : MonoBehaviour
         }
     }
 
+    void ResetBtn()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            guidePed.ST_Click(JOYPED.BTN_STATE.NORMAL, i);
+        }
+    }
 
 
 }
